b90ee1d510f309bd53a2cdd11cc38ccc66d1d016
Daniel Eicher <d.eicher@mailbox.org>
2022-05-28 08:53:48 +0200

    Creating a search hook

--- src/components/projects/ProjectsList.vue
+++ src/components/projects/ProjectsList.vue
@@ -21,9 +21,10 @@
 </template>
 
 <script>
-import { ref, computed, watch, toRefs } from "vue";
+import { computed, watch, toRefs } from "vue";
 
 import ProjectItem from "./ProjectItem.vue";
+import useSearch from "../../hooks/search.js";
 
 export default {
   components: {
@@ -31,50 +32,33 @@ export default {
   },
   props: ["user"],
   setup(props) {
-    const enteredSearchTerm = ref("");
-    const activeSearchTerm = ref("");
+    const { user } = toRefs(props);
 
-    const availableProjects = computed(function () {
-      if (activeSearchTerm.value) {
-        return props.user.projects.filter((prj) =>
-          prj.title.includes(activeSearchTerm.value)
-        );
-      }
-      return props.user.projects;
+    const projects = computed(function () {
+      return user.value ? user.value.projects : [];
     });
+
+    const { enteredSearchTerm, availableItems, updateSearch } = useSearch(
+      projects,
+      "title"
+    );
 
     const hasProjects = computed(function () {
-      return props.user.projects && availableProjects.value.length > 0;
+      return props.user.projects && availableItems.value.length > 0;
     });
-
-    watch(enteredSearchTerm, function (newValue) {
-      setTimeout(() => {
-        if (newValue === enteredSearchTerm.value) {
-          activeSearchTerm.value = newValue;
-        }
-      }, 300);
-    });
-
-    // const propsWithRefs = toRefs(props);
-    // const user = propsWithRefs.user;
-
-    const { user } = toRefs(props);
 
     watch(user, function () {
       enteredSearchTerm.value = "";
     });
 
-    function updateSearch(val) {
-      enteredSearchTerm.value = val;
-    }
-
     return {
       enteredSearchTerm,
-      availableProjects,
+      availableProjects: availableItems,
       hasProjects,
       updateSearch,
     };
   },
+
   // data() {
   //   return {
   //     enteredSearchTerm: '',
